d10528eed1dee8d1b9906125529fb50d22f0f828
pierremaeckereel <pierre.maeckereel@telecomnancy.eu>
2017-09-12 13:24:04 +0200

    page d'accueil avec informations de la base

--- home/views.py
+++ home/views.py
@@ -6,9 +6,30 @@ from django.http import *
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import TemplateView
 from django.conf import settings
+from models import *
 
 def home(request):
-    return HttpResponse("Page d'accueil ERP TNS")
+    template_name = 'home/home.html'
+
+    email = None
+    if request.user.is_authenticated():
+        email = request.user.username
+    user = Personne.objects.get(email = email)
+
+    def get_name():
+        return user.nom
+    def get_firstname():
+        return user.prenom
+    def get_email():
+        return user.email
+    def get_telephone():
+        return user.telephone
+    def get_poste():
+        return user.poste
+
+    return render(request, template_name, {'nom': get_name(), 'prenom' : get_firstname(),
+     'email' : get_email(), 'telephone' : get_telephone(), 'poste' : get_poste()})
+
 
 def logFail(request):
     return HttpResponse("Erreur d'authentification. \n Identifiant ou mot de passe incorrect")
