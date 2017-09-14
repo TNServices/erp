551c4ee2d599f467aa1571c6091f01582aa16547
pierremaeckereel <pierre.maeckereel@telecomnancy.eu>
2017-09-14 18:10:17 +0200

    ajout du style CSS

--- ERP/urls.py
+++ ERP/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 from home.views import *
 from impression.views import *
 from tresorerie.views import *
