86898a5b3dc0f560e0bbf622ec50032282463fa6
Aurelien <aurelien.benoit@telecomnancy.net>
2017-11-16 15:51:34 +0100

    Passage python3

--- impression/admin.py
+++ impression/admin.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from models import *
+from impression.models import *
 
 # Permet d'administrer la table Impression depuis l'interface d'administration
 admin.site.register(Impression)
